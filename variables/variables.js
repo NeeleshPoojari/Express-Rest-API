c17aca6459c37cb5f46c926fe6faef3fee2c1e2f
Neelesh Poojari <npoojari@sapient.com>
2019-04-03 12:53:01 +0530

    procfile updated6

---
+++ variables/variables.js
@@ -0,0 +1,3 @@
+module.exports = {
+    dbUrl:'mongodb://neel123:neel123@mycluster-shard-00-00-lwezh.mongodb.net:27017,mycluster-shard-00-01-lwezh.mongodb.net:27017,mycluster-shard-00-02-lwezh.mongodb.net:27017/test?ssl=true&replicaSet=myCluster-shard-0&authSource=admin&retryWrites=true', 
+}
